019bf7baa27074ccdc5e68a5f1201a85a2a9d0b0
AntonioDMV <Antonio-DMV@outlook.com>
2023-01-26 00:27:55 -0500

    cambios5

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -66,15 +66,27 @@ const PokeCard = ({ url }) => {
         <div className='hello'>
             <div className='card' onClick={() => navigate(`/characters/${card.id}`)} style={{ background: changeColor() }}>
                 <div>
-                    <img src={card.sprites?.other.dream_world.front_default} alt="" />
+                    <img  className='cardimg' src={card.sprites?.other.dream_world.front_default} alt="" />
+                    <div className='stats'>
+                        <div className='name-type'>
+                            <h1>{card.name}</h1>
+                            <p>{card.types?.[0]?.type.name}/{card.types?.[1]?.type.name} <br /> Type: </p> 
+                            <br />
+                            <hr /> 
+                        </div>
+                        <div className='stadistics'>
+                            <p>{card.stats?.[0].stat?.name} <br /> {card.stats?.[0].base_stat}</p> 
+                            <p>{card.stats?.[1].stat?.name} <br />{card.stats?.[1].base_stat}</p>
+                            <p>{card.stats?.[2].stat?.name} <br />{card.stats?.[2].base_stat}</p>
+                            <p>{card.stats?.[5].stat?.name} <br />{card.stats?.[5].base_stat}</p>
+                        </div>
+                    </div>
                 </div>
-                    <h1>{card.name}</h1>
-                  <p> Type: {card.types?.[0]?.type.name} / {card.types?.[1]?.type.name}</p>
+                
+                
+                    
                
-                    <p>{card.stats?.[0].stat?.name}: {card.stats?.[0].base_stat}</p> 
-                    <p>{card.stats?.[1].stat?.name}: {card.stats?.[1].base_stat}</p>
-                    <p>{card.stats?.[2].stat?.name}: {card.stats?.[2].base_stat}</p>
-                    <p>{card.stats?.[5].stat?.name}: {card.stats?.[5].base_stat}</p>
+                    
                     
                     
               
